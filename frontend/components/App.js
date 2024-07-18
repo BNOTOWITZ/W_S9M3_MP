8f6fd59211ba5c8dde303f72690aaa00ff9d7386
Brandon Notowitz <Bnotowitz@gmail.com>
2024-07-18 18:47:39 -0400

    modified:   frontend/components/App.js 	new file:   hooks/useDarkMode.js 	new file:   hooks/useLocalStorage.js 	modified:   package-lock.json git commit -m "frontend components: App.js, useDarkMode.js, useLocalStorage.js"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import data from '../data/data.json'
 import axios from "axios"
-
 import Charts from "./Charts"
 import Navbar from "./Navbar"
 
@@ -9,6 +8,7 @@ const App = () => {
   const [coinData, setCoinData] = useState([])
   const [darkMode, setDarkMode] = useState(false)
 
+  
   useEffect(() => {
     axios
       .get(
